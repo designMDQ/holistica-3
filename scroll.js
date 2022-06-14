7007b8145400f17bddcc45614754f1ba00a03bec
Charly <84675367+designMDQ@users.noreply.github.com>
2022-06-14 18:23:52 -0300

    holistica-3

---
+++ scroll.js
@@ -0,0 +1,21 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.alquimia', {
+    duration: 4000,
+    origin: 'right',
+    distance: '-100px'
+});
+
+sr.reveal('.alquimia-texto', {
+    duration: 4000,
+    origin: 'left',
+    distance: '-100px'
+});
+
+sr.reveal('.alquimia-footer', {
+    duration: 4000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+
